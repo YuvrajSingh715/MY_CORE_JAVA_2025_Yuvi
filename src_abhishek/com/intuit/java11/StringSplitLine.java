077b35d0c249f6ff1f050aa75269a4d234c9ae05
Abhishek Singh Kushwah <abhisheksinghk737@gmail.com>
2025-12-11 11:45:51 +0530

    java 11 feature

---
+++ src_abhishek/com/intuit/java11/StringSplitLine.java
@@ -0,0 +1,24 @@
+package com.intuit.java11;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class StringSplitLine {
+
+	public static void main(String[] args) {
+		String s = "A\nB\nC"; // Splits string into lines.
+		s.lines().forEach(System.out::println);
+		// repeat()
+
+		// Repeats a string N times.
+
+		System.out.println("Abhishek ".repeat(5));
+
+		// Files.readString() and Files.writeString()
+
+		// Before Java 11 → reading/writing required multiple lines.
+		
+		
+	}
+
+}
